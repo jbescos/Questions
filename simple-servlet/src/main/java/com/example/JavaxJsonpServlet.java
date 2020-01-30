c568671e9bb68c36d961cd2c617cfe0221e5a22e
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2020-01-30 14:51:29 +0100

    Adding simple servlet

---
+++ simple-servlet/src/main/java/com/example/JavaxJsonpServlet.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.json.spi.JsonProvider;
+
+public class JavaxJsonpServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        pw.println("<html><body>");
+        pw.println("Json Provider is: " + JsonProvider.provider().getClass().getName());
+        pw.println("</body></html>");
+        pw.close();
+    }
+
+}
