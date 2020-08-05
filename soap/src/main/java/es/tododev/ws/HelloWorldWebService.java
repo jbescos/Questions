f667c85832965c1d74d1961a36b5340c54fd71ae
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2020-08-05 19:46:45 +0200

    With AsyncWebservices

--- soap/src/main/java/es/tododev/ws/HelloWorldWebService.java
+++ soap/src/main/java/es/tododev/ws/HelloWorldWebService.java
@@ -3,9 +3,11 @@ package es.tododev.ws;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.xml.ws.soap.Addressing;
+import oracle.webservices.annotations.async.AsyncWebService;
 
 @Addressing(required = false)
 @WebService(name = "HelloWorldPortType", serviceName = "HelloWorldWebService")
+@AsyncWebService
 public class HelloWorldWebService {
 
     @WebMethod
