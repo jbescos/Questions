5e979e1b1cd48a6d4c00afeec7f77d824081a694
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-02-03 15:52:08 +0100

    Move to maven
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

---
+++ yassonModuleIssue/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+open module es.tododev.yasson {
+    exports es.tododev.yasson;
+    requires jakarta.json;
+    requires jakarta.json.bind;
+    requires org.eclipse.jetty.server;
+    requires jakarta.ws.rs;
+    requires org.eclipse.jetty.servlet;
+    requires jersey.media.json.binding;
+    requires jersey.common;
+    requires java.logging;
+    requires jersey.server;
+    requires jersey.container.servlet.core;
+
+}
