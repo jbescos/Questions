a85593a8d026aac95b38d3e0a12796abafa5fec9
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-02-04 11:04:52 +0100

    Improvements
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- yassonBug/src/main/java/es/tododev/yassonbug/Main.java
+++ yassonBug/src/main/java/es/tododev/yassonbug/Main.java
@@ -4,10 +4,20 @@ import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 
 public class Main {
+
     public static void main(String[] args) {
         Jsonb json = JsonbBuilder.create();
+        printModules(json.getClass().getModule(), Data.class.getModule());
         String data = "{\"prop\": \"hey\"}";
         Data deserialized = json.fromJson(data, Data.class);
         System.out.println(deserialized);
+    }
+
+    private static void printModules(Module a, Module b) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(a.getName()).append(" has access to ").append(b.getName()).append("?: ").append(a.canRead(b));
+        builder.append("\nPackages in ").append(a.getName()).append(": ").append(a.getPackages());
+        builder.append("\nPackages in ").append(b.getName()).append(": ").append(b.getPackages());
+        System.out.println(builder.toString());
     }
 }
--- yassonBug/src/main/java/module-info.java
+++ yassonBug/src/main/java/module-info.java
@@ -1,5 +1,6 @@
-open module es.tododev.yassonbug {
+module es.tododev.yassonbug {
     exports es.tododev.yassonbug;
     requires jakarta.json;
     requires jakarta.json.bind;
+    requires transitive org.eclipse.yasson;
 }
