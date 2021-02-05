d9158f3613315d49755a3be4dbb7a689c20a2fc6
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-02-05 17:45:16 +0100

    Enrich the example
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- yassonBug/src/main/java/es/tododev/yassonbug/Main.java
+++ yassonBug/src/main/java/es/tododev/yassonbug/Main.java
@@ -4,20 +4,40 @@ import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 
 public class Main {
+    
+    private static final Jsonb json = JsonbBuilder.create();
 
     public static void main(String[] args) {
-        Jsonb json = JsonbBuilder.create();
-        printModules(json.getClass().getModule(), Data.class.getModule());
-        String data = "{\"prop\": \"hey\"}";
-        Data deserialized = json.fromJson(data, Data.class);
+        
+        printModules(json.getClass(), Data.class);
+        deserializeData("{\"prop\": \"hey\"}", Data.class);
+        deserializeData("{\"name\":\"John\",\"password\":\"test\",\"email\":\"John@gmail.com\"}", Account.class);
+    }
+    
+    private static <T> void deserializeData(String data, Class<T> type) {
+        T deserialized = json.fromJson(data, type);
         System.out.println(deserialized);
+        String serialized = json.toJson(deserialized);
+        System.out.println(serialized);
     }
 
-    private static void printModules(Module a, Module b) {
+    private static void printModules(Class<?> ca, Class<?> cb) {
+        Module a = ca.getModule();
+        Module b = cb.getModule();
         StringBuilder builder = new StringBuilder();
-        builder.append(a.getName()).append(" has access to ").append(b.getName()).append("?: ").append(a.canRead(b));
-        builder.append("\nPackages in ").append(a.getName()).append(": ").append(a.getPackages());
-        builder.append("\nPackages in ").append(b.getName()).append(": ").append(b.getPackages());
+        boolean access = a.canRead(b);
+        boolean exported = a.isExported(cb.getPackageName());
+        builder.append("\n").append(a.getName()).append(" has access to ").append(b.getName()).append("?: ").append(access);
+        builder.append("\n").append(a.getName()).append(" is exported to ").append(b.getName()).append("?: ").append(exported);
+        System.out.println(builder.toString());
+        descriptor(a);
+        descriptor(b);
+    }
+    
+    private static void descriptor(Module module) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("\nDescriptor: ").append(module.getDescriptor());
+        builder.append("\nPackages: ").append(module.getPackages());
         System.out.println(builder.toString());
     }
 }
