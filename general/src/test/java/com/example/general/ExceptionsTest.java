b9f29fed37cbc2bb435087b091989ca718c13ce3
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-05-31 11:06:07 +0200

    Exception example
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

---
+++ general/src/test/java/com/example/general/ExceptionsTest.java
@@ -0,0 +1,22 @@
+package com.example.general;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ExceptionsTest {
+
+	@Test(expected = RuntimeException.class)
+	public void catchException() {
+		try {
+			Object obj = null;
+			obj.toString();
+		} catch (NullPointerException e) {
+			throw new RuntimeException(e);
+		} catch (RuntimeException e) {
+			// Do not catch the rethrown
+			fail("Should not catch previous thrown");
+		}
+	}
+	
+}
