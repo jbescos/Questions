25b73580e63704d25c1dc8f48424b7c209e7f31b
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2022-04-12 10:16:48 +0200

    Making soap example simple
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- soap/src/main/java/es/tododev/ws/HelloWorldWebService.java
+++ soap/src/main/java/es/tododev/ws/HelloWorldWebService.java
@@ -3,11 +3,9 @@ package es.tododev.ws;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.xml.ws.soap.Addressing;
-import oracle.webservices.annotations.async.AsyncWebService;
 
-@Addressing(required = false)
+//@Addressing(required = false)
 @WebService(name = "HelloWorldPortType", serviceName = "HelloWorldWebService")
-@AsyncWebService
 public class HelloWorldWebService {
 
     @WebMethod
