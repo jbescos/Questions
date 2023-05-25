c7ca7c4c20e07364e8552b1a30674be54b91dd96
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2023-05-25 10:43:19 +0200

    Improve script
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- wsgenXsdIssue/runExample.sh
+++ wsgenXsdIssue/runExample.sh
@@ -4,22 +4,36 @@ set -e
 export JAVA_HOME=~/programs/java/jdk-11.0.16.1
 export PATH=$JAVA_HOME/bin:$PATH
 CLASSPATH="classes:lib/*"
-JAVA_OPTS=""
+#JAVA_OPTS="$DEBUG"
 
-rm -rf wsGenGenerated
-rm -rf wsImportGenerated
-rm -rf schemaGenerated
-rm -rf classes
-mkdir -p wsGenGenerated
-mkdir -p wsImportGenerated
-mkdir -p schemaGenerated
-mkdir -p classes
+function compile {
+    find ./src -name "*.java" > sources.txt
+    rm -rf classes
+    mkdir -p classes
+    javac -d classes -cp $CLASSPATH @sources.txt
+}
 
-find ./src -name "*.java" > sources.txt
-javac -d classes -cp $CLASSPATH @sources.txt
+function schemaGenerated {
+    rm -rf schemaGenerated
+    mkdir -p schemaGenerated
+    java $JAVA_OPTS -cp $CLASSPATH com.sun.tools.jxc.SchemaGenerator -d schemaGenerated ./src/test/xml/*
+}
 
-java $JAVA_OPTS -cp $CLASSPATH com.sun.tools.jxc.SchemaGenerator -d schemaGenerated ./src/test/xml/*
+function wsGenGenerated {
+    rm -rf wsGenGenerated
+    mkdir -p wsGenGenerated
+    java $JAVA_OPTS -cp $CLASSPATH com.sun.tools.ws.WsGen test.ProviderMaintenanceServiceStronglyTypedEjb -wsdl -r wsGenGenerated -s wsGenGenerated -d wsGenGenerated 
+}
 
-java $JAVA_OPTS -cp $CLASSPATH com.sun.tools.ws.WsGen test.ProviderMaintenanceServiceStronglyTypedEjb -wsdl -r wsGenGenerated -s wsGenGenerated -d wsGenGenerated 
+function wsImportGenerated {
+    rm -rf wsImportGenerated
+    mkdir -p wsImportGenerated
+    java $JAVA_OPTS -cp $CLASSPATH com.sun.tools.ws.WsImport -d wsImportGenerated -keep wsGenGenerated/ProviderMaintenanceServiceStronglyTyped.wsdl
+}
 
-java $JAVA_OPTS -cp $CLASSPATH com.sun.tools.ws.WsImport -d wsImportGenerated -keep wsGenGenerated/ProviderMaintenanceServiceStronglyTyped.wsdl
+compile
+schemaGenerated
+wsGenGenerated
+wsImportGenerated
+
+
